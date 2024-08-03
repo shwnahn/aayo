fb76b8b43ff2eb1f4d860be6837503fe59689b9f
devkev00 <kimminsoo116@gmail.com>
2024-08-03 21:09:14 +0900

    [Feat] First Big Modifying

--- aayo/urls.py
+++ aayo/urls.py
@@ -1,22 +1,17 @@
-# urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'), 
-    # 호스트가 방을 생성하는 폼이 있는 화면
-    path('room/<str:unique_id>/', views.room_detail, name='room_detail'), 
-    # 호스트가 방을 생성하고 공유하기 전 확인하는 화면
-    path('join/<str:unique_id>/', views.room_view, name='room_view'), 
-    # 게스트가 초대된 방의 상세 정보를 확인하고 입장할 수 있는 화면
-    path('enter-room/<str:unique_id>/', views.enter_room, name='enter_room'), 
-    # 방에 들어가기 전에 
-    path('room/<str:unique_id>/menu/', views.room_menu, name='room_menu'), 
-    # 카페 메뉴를 크롤링해서 게스트에게 보여주는 화면
-    path('room/<str:unique_id>/confirm_menu/', views.confirm_menu, name='confirm_menu'),
-    path('room/<str:unique_id>/order/<int:order_id>/', views.order_detail, name='order_detail'),
-    # path('order/total/', views.order_total, name='order_total'),
-    path('room/<str:unique_id>/order_total/', views.order_total, name='order_total'),
-    path('room/<str:unique_id>/reset_order/', views.reset_order, name='reset_order'), 
-    # 게스트가 다시 고르기를 눌렀을 때 기존 주문 정보 삭제
+    path('', , name='main'), 
+    # 메인, 방 생성 form
+
+    path('room/<str:unique_id>/', , name='room_detail'), 
+    # 방 개요 (방 이름, 방 링크 정보 보여주기) (리디렉션(버튼) : 방 입장 / 링크 공유 / 주문 총계)
+
+    path('room/<str:unique_id>/menu/', , name='room_menu'),
+    # 방 입장 후 주문, 선택된 카페 보여주기 (로그인(이름 작성), 메뉴(크롤링) 선택 → 상세 선택(모달))
+
+    path('room/<str:unique_id>/orders/', , name='room_orders'),
+    # 주문 총계 / 메뉴 커스텀 정보 보기
 ]
