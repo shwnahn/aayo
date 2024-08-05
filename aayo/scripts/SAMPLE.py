2204d75e0565d875cf270614175e0f4febfcf07f
devkev00 <kimminsoo116@gmail.com>
2024-08-05 11:19:29 +0900

    [feat] initializing SAMPLE.py

--- aayo/scripts/SAMPLE.py
+++ aayo/scripts/SAMPLE.py
@@ -2,9 +2,9 @@ from selenium.webdriver.common.by import By # 요소 찾을 때 필요함
 from crawler import * # setup_driver, save_data 불러옴
 import time # time.sleep 쓰려고
 
-def crawl_cafename():
+def crawl_hasamdong():
     # 카페명과 메뉴URL
-    cafe_name = 'cafename'
+    cafe_name = ''
     url = ""
 
     # 웹드라이버 설정
@@ -18,7 +18,7 @@ def crawl_cafename():
 
         ###### 메뉴명과 이미지 URL 추출 - 카페마다 코드 다르게 할 부분! ######
         data = []
-        menu_items = driver.find_elements(By.CSS_SELECTOR, "li.menuDataSet")
+        menu_items = driver.find_elements(By.CLASS_NAME, "")
         
         # for문으로 메뉴 전체 순회하며 'menu_name', 'image_url'를 가져와서 data 리스트에 추가하기
         for item in menu_items:
@@ -41,4 +41,4 @@ def crawl_cafename():
 
 # 이 코드 실행 시 크롤링 함수 호출
 if __name__ == "__main__":
-    crawl_cafename()
+    crawl_hasamdong()
