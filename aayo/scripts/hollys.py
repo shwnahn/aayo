487d7b47a647da08ac5ea73e5e5f4be196a5c356
songmin0111 <violet2111@naver.com>
2024-08-06 14:12:33 +0900

    [pul]develop

--- aayo/scripts/hollys.py
+++ aayo/scripts/hollys.py
@@ -2,48 +2,71 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from crawler import * 
-import time 
+from crawler import *  # setup_driver, save_data 불러옴
+import time  # time.sleep 쓰려고
 
-def crawl_hollys():
-    # 카페명과 메뉴 URL
-    cafe_name = 'hollys'
-    url = ""
-
-    # 웹드라이버 설정
-    driver = setup_driver()
+def crawl_hollys(driver, url):
+    data = []
 
     try:
+        crawled_items = set()
         driver.get(url)
-        driver.implicitly_wait(3)
+        driver.implicitly_wait(5)  # 5초 동안 대기
+        
+        menu_items = driver.find_elements(By.CSS_SELECTOR, "ul.menu_list.line > li")
 
-        data = []
-
-        while True:
-            menu_items = driver.find_elements(By.CSS_SELECTOR, "#menu_list > li")
-
-            # for문으로 메뉴 전체 순회하며 'menu_name', 'image_url'를 가져와서 data 리스트에 추가하기
-            for item in menu_items:
-                menu_name = item.find_element(By.CSS_SELECTOR, 'div.cont_text_title > div.text1 > b').text
-                image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+        for item in menu_items:
+            print()
+            menu_name = item.find_element(By.CSS_SELECTOR, 'ul.menu_list.line > li > a > span.name').text
+            image_url = item.find_element(By.CSS_SELECTOR, 'ul.menu_list.line > li > a > span.img > img').get_attribute('src')
+                
+            if (menu_name, image_url) not in crawled_items:
+                if '빙수' in menu_name:  # 빙수 메뉴는 제외
+                    continue
                 data.append({'menu_name': menu_name, 'image_url': image_url})
-                print(menu_name, image_url)
-
+                crawled_items.add((menu_name, image_url))
+                print(f"URL: {url}, Menu: {menu_name}, Image: {image_url}")
             try:
                 more_button = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.CSS_SELECTOR, "#board_page > li > a.board_page_next"))
+                    EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/section[5]/div[2]/button'))
                 )
                 driver.execute_script("arguments[0].click();", more_button)
                 time.sleep(1)  
             except TimeoutException:
                 print("더 이상 '더보기' 버튼이 없습니다.")
                 break
+    except Exception as e:
+        print(f"크롤링 중 오류 발생: {e}")
 
-        save_data(cafe_name, data)
+    return data
+
+def main():
+    cafe_name = 'hollys'
+    all_data = []
+
+    urls = [
+        "https://www.hollys.co.kr/menu/espresso.do",
+        "https://www.hollys.co.kr/menu/signature.do",
+        "https://www.hollys.co.kr/menu/hollyccino.do",
+        "https://www.hollys.co.kr/menu/juice.do",
+        "https://www.hollys.co.kr/menu/tea.do"
+    ]
+
+    driver = setup_driver()
+
+    try:
+        for url in urls:
+            data = crawl_hollys(driver, url)
+            all_data.extend(data)
+            time.sleep(2) # 웹사이트 부하 방지
+
+        #save_data(cafe_name, all_data)
+
+    except Exception as e:
+        print(f"전체 크롤링 과정 중 오류 발생: {e}")
 
     finally:
         driver.quit()
 
-# 이 코드 실행 시 크롤링 함수 호출
 if __name__ == "__main__":
-    crawl_hollys()
+    main()
