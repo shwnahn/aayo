57f3330ac4eefb013dee97a4b3a49d59dbea9dc5
shwn <zxz8673@gmail.com>
2024-08-07 00:44:53 +0900

    [fix] update_data 코드 수정

--- aayo/update_data.py
+++ aayo/update_data.py
@@ -12,10 +12,28 @@ from aayo.models import Cafe, MenuItem
 # 파일에서 JSON 데이터 가져오기
 def load_json_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
-        return json.load(f)
+        data = json.load(f)
+        # 데이터 길이 확인
+        if len(data) <= 3:
+            print(f"Warning: JSON 파일이 너무 짧습니다. 크롤링 로직을 확인하세요! 파일: {file_path}")
+            return None
+        return data
 
 # MenuItem 객체 업데이트
 def update_menu_items(cafe, json_data):
+    if json_data is None:
+        return
+    # JSON 데이터에서 메뉴 이름 목록 가져오기
+    new_menu_names = [item['menu_name'] for item in json_data]
+    # 현재 카페의 모든 메뉴 아이템 가져오기
+    existing_menu_items = MenuItem.objects.filter(cafe=cafe)
+
+    # JSON 데이터에 없는 기존 메뉴 아이템 삭제
+    for menu_item in existing_menu_items:
+        if menu_item.name not in new_menu_names:
+            menu_item.delete()
+    
+    # JSON 데이터 기반으로 메뉴 아이템 업데이트 또는 생성
     for item in json_data:
         defaults = {
             'image_url': item['image_url'],
