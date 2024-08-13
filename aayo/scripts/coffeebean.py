53a0f8735f543968d4f8ee84b90cb94167f068d0
Jihyeon Lee <ljh130334@naver.com>
2024-08-13 10:46:21 +0900

    [pull] jihyeon/feature -> 시환 수정

--- aayo/scripts/coffeebean.py
+++ aayo/scripts/coffeebean.py
@@ -19,35 +19,122 @@ def crawl_coffeebean():
             driver.get(url)
             driver.implicitly_wait(3)
 
-            menu_items = driver.find_elements(By.CSS_SELECTOR, ".menu_list > ul > li")
+            while True:
 
-        
-            for item in menu_items:
-                try:
-                    menu_name = item.find_element(By.CLASS_NAME, 'kor').text
-                    image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
-                    
-                    print(menu_name, image_url)
 
-                    data.append({
-                        "menu_name": menu_name,
-                        "image_url": image_url,
-                    })
 
-                except Exception as e:
-                    print(f"Error processing menu item: {str(e)}")
-                    
+            # menu_items = driver.find_elements(By.CSS_SELECTOR, "ul.menu_list > li")
+            # next_button = driver.find_element(By.CSS_SELECTOR, "div.paging > a.next")
+            # children = driver.find_elements(By.CSS_SELECTOR, 'div.paging > a')
+            # pages = driver.find_elements(By.CSS_SELECTOR, "div.paging")
+            # first_child = children[0]
+            # last_child = children[-1]
+            # # print(children)
+
+            # if 'on' in first_child.get_attribute('class'):
+            #     for item in menu_items:
+            #         try:
+            #             menu_name = item.find_element(By.CLASS_NAME, 'kor').text
+            #             image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+                        
+            #             print(menu_name, image_url)
+
+            #             data.append({
+            #                 "menu_name": menu_name,
+            #                 "image_url": image_url,
+            #             })
+
+            #         except Exception as e:
+            #             print(f"Error processing menu item: {str(e)}")
+            # else:
+                
+            #     for page in page_numbers:
+            #         for item in menu_items:
+            #             if 'on' not in last_child.get_attribute('class'):
+            #                 try:
+            #                     menu_name = item.find_element(By.CLASS_NAME, 'kor').text
+            #                     image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+                                
+            #                     print(menu_name, image_url)
+
+            #                     data.append({
+            #                         "menu_name": menu_name,
+            #                         "image_url": image_url,
+            #                     })
+
+            #                 except Exception as e:
+            #                     print(f"Error processing menu item: {str(e)}")
+            #             try:
+            #                 next_button.click()
+            #                 time.sleep(2)
+            #             except Exception as e:
+            #                 print(f"No more pages or error navigating: {str(e)}")
+            #                 break
+            #             else:
+            #                 try:
+            #                     menu_name = item.find_element(By.CLASS_NAME, 'kor').text
+            #                     image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+                                
+            #                     print(menu_name, image_url)
+
+            #                     data.append({
+            #                         "menu_name": menu_name,
+            #                         "image_url": image_url,
+            #                     })
+
+            #                 except Exception as e:
+            #                     print(f"Error processing menu item: {str(e)}")
+
+            
+
+
+
+
+                # for item in menu_items:
+                #     try:
+                #         menu_name = item.find_element(By.CLASS_NAME, 'kor').text
+                #         image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+                        
+                #         print(menu_name, image_url)
+
+                #         data.append({
+                #             "menu_name": menu_name,
+                #             "image_url": image_url,
+                #         })
+
+                #     except Exception as e:
+                #         print(f"Error processing menu item: {str(e)}")
                 # try:
-                #     next_button = driver.find_element(By.CSS_SELECTOR, "div.paging > a.next")
-                #     if 'disabled' in next_button.get_attribute('class'):
-                #         break
                 #     next_button.click()
                 #     time.sleep(2)
                 # except Exception as e:
                 #     print(f"No more pages or error navigating: {str(e)}")
                 #     break
+
+            # while 'disabled' not in next_button:
+            #     for item in menu_items:
+            #         try:
+            #             menu_name = item.find_element(By.CLASS_NAME, 'kor').text
+            #             image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+                        
+            #             print(menu_name, image_url)
+
+            #             data.append({
+            #                 "menu_name": menu_name,
+            #                 "image_url": image_url,
+            #             })
+
+            #         except Exception as e:
+            #             print(f"Error processing menu item: {str(e)}")
+                    
+            #     try:
+            #         next_button.click()
+            #         time.sleep(2)
+            #     except Exception as e:
+            #         print(f"No more pages or error navigating: {str(e)}")
+            #         break
         
-        save_data(cafe_name, data)
+        # save_data(cafe_name, data)
     finally:
         driver.quit()
 
