c0aca0ac2b3546609a3bae1fc931450f5eb9a43a
Jihyeon Lee <ljh130334@naver.com>
2024-08-14 16:39:30 +0900

    Shynchronize jihyeon/feature with develop branch

--- aayo/scripts/theventi.py
+++ aayo/scripts/theventi.py
@@ -1,41 +1,40 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from crawler import *
 import time
 
 def crawl_theventi():
     cafe_name = 'theventi'
-    base_url = "https://www.theventi.co.kr/new2022/menu/all.html?mode="
+    url = "https://www.theventi.co.kr/new2022/menu/all.html?mode="
 
     driver = setup_driver()
 
     try:
         data = []
-        categories = ['1', '2', '3', '4', '5', '6', '7', '8']
+        categories = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-        for category in categories :
-            url = base_url + category
-            driver.get(url)
-            driver.implicitly_wait(3)
+        for category in categories:
+            
 
-            menu_items = driver.find_elements(By.CSS_SELECTOR, "div.menu_list > ul > li")
-            for item in menu_items:
-                try:
-                    menu_name = item.find_element(By.CSS_SELECTOR, 'p.tit').text
-                    print(menu_name)
-                    image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
-                    print(image_url)
 
-                    data.append({
-                        "menu_name": menu_name,
-                        "image_url": image_url,
-                    })
 
-                except Exception as e:
-                    print(f"Error processing menu item: {str(e)}")
+        driver.get(url)
+        driver.implicitly_wait(3)
 
-            save_data(cafe_name, data)
+        data = []
+        menu_items = driver.find_elements(By.CLASS_NAME, "")
+        
+        for item in menu_items:
+            menu_name = item.find_element(By.TAG_NAME, 'dd').text
+            image_url = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+            
+            print(menu_name, image_url)
+
+            data.append({
+                "menu_name": menu_name,
+                "image_url": image_url,
+            })
+        
+        save_data(cafe_name, data)
     finally:
         driver.quit()
 
