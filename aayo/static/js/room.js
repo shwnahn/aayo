679b1aa2684f9cf9e3adfe4c008c399a479dc494
shwn <zxz8673@gmail.com>
2024-08-17 14:31:23 +0900

    교통정리...시환

--- aayo/static/js/room.js
+++ aayo/static/js/room.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const nameInput = document.getElementById('name');
     const submitButton = document.querySelector('.submit-button')
     console.log(submitButton)
+
     function checkFormCompletion() {
         console.log("checkForm")
         if (nameInput.value.trim() !== '' && cafeInput.value !== '') {
