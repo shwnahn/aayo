7e244bb43ea5b2d2fff56257b72068dae4d2c8d4
Jihyeon Lee <ljh130334@naver.com>
2024-08-19 00:24:42 +0900

    [crawling_category] the venti

--- admin.py
+++
@@ -1,8 +0,0 @@
-# admin.py
-from django.contrib import admin
-from .models import *
-
-admin.register(Room)
-admin.site.register(GuestOrder)
-admin.site.register(Cafe)
-admin.site.register(MenuItem)
