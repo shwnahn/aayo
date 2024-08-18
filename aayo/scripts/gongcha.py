93c5248ba100f1f116992d4cb5206fc3a0873f0a
shwn <zxz8673@gmail.com>
2024-08-18 23:40:42 +0900

    [crawling] 커피빈, 공차

--- aayo/scripts/gongcha.py
+++ aayo/scripts/gongcha.py
@@ -2,18 +2,42 @@ from selenium.webdriver.common.by import By # 요소 찾을 때 필요함
 from crawler import * # setup_driver, save_data 불러옴
 import time # time.sleep 쓰려고
 
+def save_index(driver):
+    driver.get("https://www.gong-cha.co.kr/brand/menu/product.php?c=001001")
+    print("접속성공")
+    driver.implicitly_wait(3)
+    index_list = []
+    indexes = driver.find_elements(By.CSS_SELECTOR, 'div.pro_list_tab > ul > li > a')
+    print("index_list")
+    for index in indexes:
+        print("index")
+        url = index.get_attribute('href')
+        category = index.find_element(By.TAG_NAME, 'span').text
+        index_list.append({
+            "url": url,
+            "category": category
+        })
+    print(index_list)
+    return index_list
+
+
 def crawl_gongcha():
     # 카페명과 메뉴URL
     cafe_name = 'gongcha'
     # 웹드라이버 설정 
     driver = setup_driver()
 
-    index_list = ["001", "002", "003", "006", "007", "010", "011"]
-
-    try:
+    try: 
         data = []
-        for index in index_list:
-            url = f"https://www.gong-cha.co.kr/brand/menu/product.php?c=001{index}"
+        index_list = save_index(driver)
+        driver.implicitly_wait(3)
+        print("# index_list 함수 구동 완료")
+        for entry in index_list:
+            url = entry["url"]
+            category = entry["category"]
+            note = ''
+            print("url 오픈...")
+            print(url)
             # 크롤링할 페이지 열기
             driver.get(url)
             # 페이지 소스 로딩 대기 (최대 3초)
@@ -28,11 +52,15 @@ def crawl_gongcha():
                 menu_name = item.find_element(By.CSS_SELECTOR, 'span.txt').text
                 image_url = item.find_element(By.CSS_SELECTOR, 'span.img > img').get_attribute('src')
 
-                # menu_name, image_url을 data 리스트에 하나씩 추가
-                data.append({
-                    "menu_name": menu_name,
-                    "image_url": image_url,
-                })
+                if menu_name:
+                    data.append({
+                        "menu_name": menu_name,
+                        "image_url": image_url,
+                        "category": category,
+                        "note": note,
+                    })
+                    print(f"[{category}] {menu_name} - {note}")
+                    print(image_url)
                 
         # JSON 파일로 저장
         save_data(cafe_name, data)
