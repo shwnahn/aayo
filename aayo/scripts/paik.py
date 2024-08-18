078a1b2ae9756483ca3e7948a25fe04a9a7d8f1c
shwn <zxz8673@gmail.com>
2024-08-18 23:54:28 +0900

    [crawling] 빽다방

--- aayo/scripts/paik.py
+++ aayo/scripts/paik.py
@@ -2,12 +2,18 @@ from selenium.webdriver.common.by import By
 from crawler import *
 import time
 
-def crawl_paik(driver, url, is_new_menu=False):
+def test():
+    driver = setup_driver()
+    driver.get("https://paikdabang.com/menu/menu_coffee/")
+
+def crawl_paik(driver, url, category, is_new_menu=False):
+    print(category)
     data = []
 
     try:
         driver.get(url)
         driver.implicitly_wait(5)  # 5초 동안 대기
+        note = ''
 
         if is_new_menu:
             menu_items = driver.find_elements(By.CSS_SELECTOR, "div.swiper-slide.swiper-slide-active, div.swiper-slide.swiper-slide-next")
@@ -25,10 +31,15 @@ def crawl_paik(driver, url, is_new_menu=False):
                 
                 print(f"메뉴명: {menu_name}, 이미지 URL: {image_url}")
 
-                data.append({
-                    "menu_name": menu_name,
-                    "image_url": image_url,
-                })
+                if menu_name:
+                    data.append({
+                        "menu_name": menu_name,
+                        "image_url": image_url,
+                        "category": category,
+                        "note": note,
+                    })
+                    print(f"[{category}] {menu_name} - {note}")
+                    print(image_url)
             except Exception as e:
                 print(f"메뉴 항목 처리 중 오류 발생: {e}")
 
@@ -42,18 +53,20 @@ def main():
     all_data = []
 
     urls = [
-        "https://paik.com/menu/menu_new/",
-        "https://paik.com/menu/menu_coffee/",
-        "https://paik.com/menu/menu_drink/",
-        "https://paik.com/menu/menu_ccino/"
+        {"url": "https://paikdabang.com/menu/menu_new/", "category": "신메뉴"},
+        {"url": "https://paikdabang.com/menu/menu_coffee/", "category": "커피"},
+        {"url": "https://paikdabang.com/menu/menu_drink/", "category": "음료"},
+        {"url": "https://paikdabang.com/menu/menu_ccino/", "category": "빽스치노"}
     ]
 
     driver = setup_driver()
 
     try:
-        for url in urls:
+        for entry in urls:
+            url = entry["url"]
+            category = entry["category"]
             is_new_menu = 'menu_new' in url
-            data = crawl_paik(driver, url, is_new_menu)
+            data = crawl_paik(driver, url, category, is_new_menu)
             all_data.extend(data)
             time.sleep(2) # 웹사이트 부하 방지
 
@@ -66,4 +79,5 @@ def main():
         driver.quit()
 
 if __name__ == "__main__":
+    # test()
     main()
